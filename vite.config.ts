daf516a2289e2a8e16a1075c1cbf400bb33fe7b9
Monir <182096090+mo-kdd@users.noreply.github.com>
2024-09-24 19:23:46 +0200

    base update to match repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/pathfinder-react/",
+    base: "/bfs-pathfinder-react/",
 });
